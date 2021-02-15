08e915f8b027cf8affd56b0a9df84ebd00a4d7e1
Miguel Morales <minesto23@gmail.com>
2021-02-14 22:33:55 -0400

    primer commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img
+          src="https://logrocket-assets.io/img/logo.png"
+          width="300"
+          className="img-thumbnail"
+          style={{ marginTop: "20px" }}
+        />
+        <hr />
+        <h5>
+          <i>presents</i>
+        </h5>
+        <h1>App with React + Django</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
